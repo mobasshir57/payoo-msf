df7be759a0efe96c9d2d25afa7bb2fa480e61b7a
Md Mobasshir Ahmed <mobasshirahmed646@gmail.com>
2026-04-08 03:00:40 +0600

    home page and cashout section designed

---
+++ script/cashout.js
@@ -0,0 +1,11 @@
+document.getElementById('cashout-btn').addEventListener("click", function(){
+// 1- get the agent number and validate
+// 2- get the ammount, validate, convert to number
+// 3- get the current balance, validate, convert to number
+// 4- Calculate new balance
+// 5- get pin and verify
+// 5.1- true:: show an alert and set balance
+// 5.2- False:: show an error alart and return
+
+
+})
